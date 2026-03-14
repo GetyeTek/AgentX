99a88e4612f0673d613953debb04691041cfa1ed
GetyeTek <getyeteklu2@gmail.com>
2026-03-14 15:00:14 +0300

    Conduit: 1 ops

--- app/src/main/java/com/agentx/app/DebugLogManager.kt
+++ app/src/main/java/com/agentx/app/DebugLogManager.kt
@@ -13,7 +13,10 @@ object DebugLogManager {
     fun log(tag: String, message: String) {
         val timestamp = SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault()).format(Date())
         val entry = "[$timestamp] $tag: $message"
-        _logs.value = _logs.value + entry
+        val current = _logs.value.toMutableList()
+        if (current.size > 200) current.removeAt(0)
+        current.add(entry)
+        _logs.value = current
     }
 
     fun clear() {
